3f97677b1f94b4d6b48a6535b3a744e9c3cd6542
Robert Njane <robert.njane@andela.com>
2018-11-01 19:37:21 +0300

    ch(models): Add application models and associated migrations

--- BucketLists/bucket_lists_app/models.py
+++ BucketLists/bucket_lists_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class BucketListsModel(models.Model):
+    name = models.CharField(max_length=30)
+    owner = models.ForeignKey(User, related_name='bucketlists', on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['date_created']
+
+class BucketItemsModel(models.Model):
+    name = models.CharField(max_length=30)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    bucketlist = models.ForeignKey(BucketListsModel, related_name='bucket_items', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['date_created']
